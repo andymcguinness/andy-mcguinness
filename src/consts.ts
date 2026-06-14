ffaa2f9c1269a8b34bfc81229af92640a5cd6939
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2026-06-14 10:51:29 -0400

    feat: Updating content to reflect current goals.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Andy McGuinness | Multi-Hyphenate Creative Dude';
-export const SITE_DESCRIPTION = 'Developer, Twitch streamer, DJ, musician, barista, and content creator based in NW Ohio. Explore my projects, blog posts, and work.';
+export const SITE_TITLE = 'Andy McGuinness | Support Engineer & Developer';
+export const SITE_DESCRIPTION = 'Software support engineer and full-stack developer with 10+ years of experience in SaaS. Based in NW Ohio.';
